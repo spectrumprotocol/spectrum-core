dc1fd65a25bc4702ec848aaab9ad2bbbf58ae3dd
hikari042 <spectrum.hikari@gmail.com>
2022-10-25 03:56:43 +0700

    add lp staking contract

--- packages/spectrum/src/lib.rs
+++ packages/spectrum/src/lib.rs
@@ -1,6 +1,7 @@
+pub mod adapters;
 pub mod astroport_farm;
 pub mod compound_proxy;
 pub mod fees_collector;
-pub mod pair_proxy;
 pub mod helper;
-pub mod adapters;
+pub mod pair_proxy;
+pub mod lp_staking;
